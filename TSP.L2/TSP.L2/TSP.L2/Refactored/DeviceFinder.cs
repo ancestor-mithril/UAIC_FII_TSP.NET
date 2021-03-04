8e6ffc62e5ac3dbd50893e243b5b1dbdfc913cac
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2021-03-04 14:16:44 +0200

    (Lab 2) Inital refactoring
     - refactored SingleChoice
     - refactored DeviceFinder
     - todo: check DeviceFinder refactoring and maybe add PortFinderFactory

---
+++ TSP.L2/TSP.L2/TSP.L2/Refactored/DeviceFinder.cs
@@ -0,0 +1,41 @@
+﻿using SOLID.OCP;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+namespace TSP.L2.Refactored
+{
+    public class DeviceFinder
+    {
+        private readonly Dictionary<DeviceModel, ISerialPortFinder> portFinder;
+        public DeviceFinder()
+        {
+            portFinder.Add(DeviceModel.BillAccepterCashCode, new PortFinderCashCode());
+            portFinder.Add(DeviceModel.BillDispenserEcdm, new PortFinderEcdm());
+            portFinder.Add(DeviceModel.CoinAccepterNri, new PortFinderNri());
+            portFinder.Add(DeviceModel.CoinDispenserCube4, new PortFinderCube4());
+            portFinder.Add(DeviceModel.CoinDispsenerSch2, new PortFinderSch2());
+        }
+        public string FindDevice(DeviceModel model)
+        {
+            if (portFinder.ContainsKey(model))
+            {
+                var port = portFinder[model].FindPort();
+                return Find(port);
+            }
+            throw new ArgumentException($"Unknown model: {model}.");
+        }
+
+        private string Find(SerialPort port)
+        {
+            string[] names = SerialPort.GetPortNames();
+            foreach (string name in names)
+            {
+                port.Write("special code");
+                if (port.ReadByte() == 0)
+                    return name;
+            }
+            return null;
+        }
+    }
+}
---
+++ TSP.L2/TSP.L2/TSP.L2/Refactored/SingleChoice.cs
@@ -0,0 +1,29 @@
+﻿using SOLID.OCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP.L2.Refactored
+{
+    public class BankTerminalFactory
+    {
+        private static readonly Dictionary<BankTerminalModel, Func<IBankTerminal>> bankTerminals;
+
+        public BankTerminalFactory()
+        {
+            bankTerminals.Add(BankTerminalModel.Brp, () => new BrpTerminal());
+            bankTerminals.Add(BankTerminalModel.Dcp, () => new DcpTerminal());
+        }
+
+        public static IBankTerminal CreateBankTerminal(BankTerminalModel model)
+        { 
+            if (bankTerminals.ContainsKey(model))
+            {
+                return bankTerminals[model]();
+            }
+            throw new ArgumentException("Unknown model");
+        }
+    }
+}
