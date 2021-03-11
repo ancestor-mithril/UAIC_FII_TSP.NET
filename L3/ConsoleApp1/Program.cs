35f8401061788eca64bd59494606f3c91b8eb647
unknown <engineeredbase484@gmail.com>
2021-03-11 12:40:31 +0200

    L3 (poate bun)

---
+++ L3/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using SOLID.LSP.Refactored;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patrulater rect = new Rectangle() { Width = 2, Height = 5 };
+            int rectArea = AreaCalculator.CalcPatrulater(rect);
+
+            Console.WriteLine($"Rectangle Area = {rectArea}");
+
+            //
+
+            Patrulater square = new Square() { Height = 2, Width = 10 };
+            int squareArea = AreaCalculator.CalcPatrulater(square);
+            Console.WriteLine($"Square Area = {squareArea}");
+        }
+    }
+}
---
+++ L3/LSP/Refactored/Refactored.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace SOLID.LSP.Refactored
+{
+    public abstract class Patrulater
+    {
+        public virtual int Height { get; set; }
+        public virtual int Width { get; set; }
+    }
+
+    public class Rectangle : Patrulater
+    {
+        public override int Width { get; set; }
+        public override int Height { get; set; }
+        public int Area
+        {
+            get
+            {
+                return Height * Width;
+            }
+        }
+    }
+
+    public class Square : Patrulater
+    {
+        private int _size;
+        public override int Height
+        {
+            get
+            {
+                return _size;
+            }
+            set
+            {
+                _size = value;
+            }
+        }
+        public override int Width
+        {
+            get
+            {
+                return _size;
+            }
+            set
+            {
+                _size = value;
+            }
+        }
+        public int Area
+        {
+            get
+            {
+                return _size * _size;
+            }
+        }
+    }
+
+    public class AreaCalculator
+    {
+        public static int CalcPatrulater(Patrulater pat) => pat.Height * pat.Width;
+    }
+
+    class EntryPoint
+    {
+        static void SuperMain(string[] args)
+        {
+            Patrulater rect = new Rectangle() { Width = 2, Height = 5 };
+            int rectArea = AreaCalculator.CalcPatrulater(rect);
+
+            Console.WriteLine($"Rectangle Area = {rectArea}");
+
+            //
+
+            Patrulater square = new Square() { Height = 2, Width = 10 };
+            int squareArea = AreaCalculator.CalcPatrulater(square);
+            Console.WriteLine($"Square Area = {squareArea}");
+        }
+    }
+}
